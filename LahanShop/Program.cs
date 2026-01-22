c36565bd9b11568acbb0ae457e1672ab5dbd068b
Lahan <romanlagan3@gmail.com>
2026-01-22 22:07:20 +0200

    feat: configure CORS for React

--- LahanShop/Program.cs
+++ LahanShop/Program.cs
@@ -8,9 +8,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<LahanShop.Data.AppDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactApp", policyBuilder =>
+    {
+        policyBuilder.WithOrigins("http://localhost:5173") // Адреса майбутнього React-додатка
+                     .AllowAnyHeader()
+                     .AllowAnyMethod();
+    });
+});
 
 
 var app = builder.Build();
@@ -25,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("ReactApp");
 
 app.UseAuthorization();
 

cc59866d3c3e1f791679a7481a2879240a725c01
Lahan <romanlagan3@gmail.com>
2026-02-14 19:21:13 +0200

    fix

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://lahanshop-api.azurewebsites.net';
+export const API_BASE_URL = 'https://lahanshop-api-gnddbbf6fuapefee.swedencentral-01.azurewebsites.net';
 
 export const CURRENCY_FORMATTER = new Intl.NumberFormat('uk-UA', {
   style: 'currency',
